ef28c57af96c0c1e22c31cb5ec9a7edccd54b5c3
Sergey Manzhos <opghld@gmail.com>
2022-12-14 12:34:18 +0200

    Update setup-root-ssh.sh

--- setup-root-ssh.sh
+++ setup-root-ssh.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "PermitRootLogin yes" | tee -a /etc/ssh/sshd_config
 service sshd restart
+systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 echo "Don't forget set password for root"
